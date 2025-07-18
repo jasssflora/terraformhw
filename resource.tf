23bf40478a2ecef635113655ce6e67f079c65688
jk.karan375@gmail.com <jk.karan375@gmail.com>
2025-07-18 02:54:57 +0000

    RG

--- resource.tf
+++ resource.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "rg" {
   name     = "myResourceGroup"
   location = "East US"
 }
+resource "azurerm_storage_account" "storage" {
+  name                     = "jassi23"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
